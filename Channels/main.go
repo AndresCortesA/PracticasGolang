4a1e58168b040ea8f0403603788d780fc3d8d6c2
Andres <andrescortes.22311@gmail.com>
2023-01-30 16:19:32 -0500

    Channels terminado con concurrencias

--- Channels/main.go
+++ Channels/main.go
@@ -1,14 +1,20 @@
 package main
 
 /*
-supongamos que tenemos una gourutine en el channel A[] el cual quiere compartir su informacion
+supongamos que tenemos una gorutine en el channel A[] el cual quiere compartir su informacion
 A[] INFO -> CHANNEL
-ahora tenemos otra gourutine B[] la cual quiere conocer la informacion del channel
+ahora tenemos otra gorutine B[] la cual quiere conocer la informacion del channel
 B[] CHANNEL -> INFO
 
-asi se conparte la informacion de una gourutine a otra solo desde el channel, osea no se comparten la
+asi se conparte la informacion de una gorutine a otra solo desde el channel, osea no se comparten la
 misma informacion
 
+tipos de channels:
+bidireccionales - envia y recibe datos
+receive-only - solo recibe
+send-only solo envia
+
+sin embargo podemos indicarle a cada channel que tendra un comportamiento en especifico
 
 */
 import (
@@ -21,16 +27,31 @@ import (
 func main() {
 	wg := &sync.WaitGroup{}
 	IDsChan := make(chan string)
+	fakeIdsChan := make(chan string)
+	closedChans := make(chan int)
 	//Cuantas gourutines vamos a esperar eso, se soluciona con wg.add()
-	wg.Add(2)
+	wg.Add(3)
 
-	go generadorDeID(wg, IDsChan)
-	go logIDS(wg, IDsChan)
+	go generadorDeID(wg, IDsChan, closedChans)
+	go generadorFakeID(wg, fakeIdsChan, closedChans)
+
+	go logIDS(wg, IDsChan, fakeIdsChan, closedChans)
 
 	wg.Wait()
 }
 
-func generadorDeID(wg *sync.WaitGroup, idsChan chan string) {
+func generadorFakeID(wg *sync.WaitGroup, fakeIdsChan chan<- string, closedChans chan<- int) {
+	for i := 0; i < 50; i++ {
+		id := uuid.New()
+		fakeIdsChan <- fmt.Sprintf("%d. %s", i+1, id.String())
+	}
+	close(fakeIdsChan)
+	closedChans <- 1
+	wg.Done()
+}
+
+//solo recibe <-
+func generadorDeID(wg *sync.WaitGroup, idsChan chan<- string, closedChans chan<- int) {
 
 	for i := 0; i < 100; i++ {
 		id := uuid.New()
@@ -38,15 +59,42 @@ func generadorDeID(wg *sync.WaitGroup, idsChan chan string) {
 		idsChan <- fmt.Sprintf("%d. %s", i+1, id.String())
 	}
 	close(idsChan)
-
+	closedChans <- 1
 	wg.Done()
 }
 
-func logIDS(wg *sync.WaitGroup, idsChan chan string) {
+func logIDS(wg *sync.WaitGroup, idsChan <-chan string, fakeIdsChan <-chan string, closedChans chan int) {
 	//y aqui recibimos la variable idsChan hacia la variable id
-	for id := range idsChan {
-		fmt.Println(id)
-	}
+	//	for id := range idsChan {
+	//		fmt.Println(id)
+	//	}
 
+	closeCounter := 0
+
+	for {
+		select {
+		case id, abierto := <-idsChan:
+			if abierto {
+				fmt.Println("ID", id)
+			}
+
+		case id, abierto := <-fakeIdsChan:
+			if abierto {
+				fmt.Println("FAKEID", id)
+			}
+
+		case count, abierto := <-closedChans:
+			if abierto {
+				closeCounter += count
+			}
+		}
+
+		if closeCounter == 2 {
+			close(closedChans)
+			break
+		}
+
+	}
 	wg.Done()
+
 }
